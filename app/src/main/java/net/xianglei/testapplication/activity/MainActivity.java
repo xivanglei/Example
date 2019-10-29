4192e24684450c2b264c6245fdc7901ec244f255
xianglei <xianglei19881119@163.com>
2019-10-29 12:11:56 +0800

    加入自定义web正则的textView

--- app/src/main/java/net/xianglei/testapplication/activity/MainActivity.java
+++ app/src/main/java/net/xianglei/testapplication/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package net.xianglei.testapplication.activity;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +23,7 @@ import net.xianglei.testapplication.component.jncryptor.JNCryptor;
 import net.xianglei.testapplication.utils.CommonUtil;
 import net.xianglei.testapplication.utils.GlideUtil;
 import net.xianglei.testapplication.utils.LogUtil;
+import net.xianglei.testapplication.utils.StringUtil;
 
 import java.net.URL;
 import java.net.URLConnection;
@@ -29,10 +34,10 @@ import butterknife.OnClick;
 
 public class MainActivity extends SimpleActivity {
 
-    @BindView(R.id.iv_skip)
-    ImageView iv_skip;
     @BindView(R.id.btn_test2)
     Button btn_test2;
+    @BindView(R.id.tv_test)
+    TextView tv_test;
 
     private String mImageUrl = "https://rongcloud-file.cn.ronghub.com/application_octet-stream__RC-2019-10-21_8228_1571652938959.mp4?attname=40dfc95cc9fb4ba497b445d10441782f.mp4&e=2147483647&token=CddrKW5AbOMQaDRwc3ReDNvo3-sL_SO1fSUBKV3H:YV8sUH1aAnYaVrh-7iJx0iB31N0=";
 
@@ -43,18 +48,17 @@ public class MainActivity extends SimpleActivity {
 
     @Override
     protected void initViewAndData(Bundle savedInstanceState) {
-        GlideUtil.loadImage(this, mImageUrl, iv_skip);
-        btn_test2.getLayoutParams().width = 500;
 
     }
 
     @OnClick(R.id.btn_test)
     public void test() {
-        new Thread(() -> CommonUtil.getHttpFileLength(mImageUrl)).start();
+        tv_test.setText("lkjlkjlkjhttp://www.baidu.com");
     }
 
     @OnClick(R.id.btn_test2)
     public void test2() {
+
     }
 
     @OnClick(R.id.btn_get_address_list)
@@ -92,8 +96,8 @@ public class MainActivity extends SimpleActivity {
     }
 
     @OnClick(R.id.btn_slide_close_layout)
-    public void startSlideCloseActivity() {
-        SlideClosePictureActivity.start(this, mImageUrl, iv_skip);
+    public void startSlideCloseActivity(View v) {
+        SlideClosePictureActivity.start(this, mImageUrl, v);
     }
 
 
---
+++ app/src/main/java/net/xianglei/testapplication/widget/AutoWebLinkTextView.java
@@ -0,0 +1,50 @@
+package net.xianglei.testapplication.widget;
+
+import android.content.Context;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Author:xianglei
+ * Date: 2019-10-29 11:06
+ * Description:
+ */
+public class AutoWebLinkTextView extends android.support.v7.widget.AppCompatTextView {
+
+    public AutoWebLinkTextView(Context context) {
+        super(context);
+    }
+
+    public AutoWebLinkTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoWebLinkTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private final String regex = "(https|http):\\/\\/[-a-zA-Z0-9+&@#\\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\\/%=~_|]";
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        String str = text.toString();
+
+        SpannableString spannableString = new SpannableString(str);
+        Matcher urlMatcher = Pattern.compile(regex).matcher(str);
+        while (urlMatcher.find()) {
+            String url = urlMatcher.group();
+            int start = urlMatcher.start();
+            int end = urlMatcher.end();
+            spannableString.setSpan(new URLSpan(url), start, end, 0);
+        }
+
+        super.setText(spannableString, type);
+        super.setMovementMethod(new LinkMovementMethod());
+    }
+}
