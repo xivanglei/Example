d25b0bb69f1dd633af1ce8190ec3fd3010eaf83e
xianglei <xianglei19881119@163.com>
2019-10-30 09:31:22 +0800

    完成图片压缩

--- app/src/main/java/net/xianglei/testapplication/activity/MainActivity.java
+++ app/src/main/java/net/xianglei/testapplication/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package net.xianglei.testapplication.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Base64;
@@ -22,14 +24,19 @@ import net.xianglei.testapplication.component.jncryptor.CryptorException;
 import net.xianglei.testapplication.component.jncryptor.JNCryptor;
 import net.xianglei.testapplication.utils.CommonUtil;
 import net.xianglei.testapplication.utils.GlideUtil;
+import net.xianglei.testapplication.utils.ImageCompressUtil;
 import net.xianglei.testapplication.utils.LogUtil;
+import net.xianglei.testapplication.utils.SDCardUtil;
 import net.xianglei.testapplication.utils.StringUtil;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import top.zibin.luban.Luban;
 
 
 public class MainActivity extends SimpleActivity {
@@ -38,9 +45,12 @@ public class MainActivity extends SimpleActivity {
     Button btn_test2;
     @BindView(R.id.tv_test)
     TextView tv_test;
+    @BindView(R.id.iv_test)
+    ImageView iv_test;
 
     private String mImageUrl = "https://rongcloud-file.cn.ronghub.com/application_octet-stream__RC-2019-10-21_8228_1571652938959.mp4?attname=40dfc95cc9fb4ba497b445d10441782f.mp4&e=2147483647&token=CddrKW5AbOMQaDRwc3ReDNvo3-sL_SO1fSUBKV3H:YV8sUH1aAnYaVrh-7iJx0iB31N0=";
 
+    private String srcFile = "/storage/emulated/0/DCIM/Camera/IMG_20191004_150413.jpg";
     @Override
     protected int getLayoutById() {
         return R.layout.activity_main;
@@ -53,12 +63,31 @@ public class MainActivity extends SimpleActivity {
 
     @OnClick(R.id.btn_test)
     public void test() {
-        tv_test.setText("lkjlkjlkjhttp://www.baidu.com");
+        long start = System.currentTimeMillis();
+//        try {
+//            GlideUtil.loadImage(this, Luban.with(this)
+//                    .load(new File(srcFile))
+//                    .ignoreBy(100)
+//                    .setTargetDir(SDCardUtil.getPrivateCacheDir().getAbsolutePath())
+//                    .get().get(0).getAbsolutePath(), iv_test);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        LogUtil.d(System.currentTimeMillis() - start);
+        File file = new File(srcFile);
+        try {
+            File file1 = ImageCompressUtil.compress(file, SDCardUtil.getPrivateCacheDir().getAbsolutePath(),  500);
+            LogUtil.d(System.currentTimeMillis() - start);
+            GlideUtil.loadImage(this, file1.getAbsolutePath(), iv_test);
+            LogUtil.d(file1.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @OnClick(R.id.btn_test2)
     public void test2() {
-
+        GlideUtil.loadImage(this, srcFile, iv_test);
     }
 
     @OnClick(R.id.btn_get_address_list)
