fc35e641aaf34a8fc91983eb68c3c0201a394695
xianglei <xianglei19881119@163.com>
2019-12-14 15:27:29 +0800

    储存

---
+++ app/src/main/java/com/dalongtech/testapplication/app/App.java
@@ -0,0 +1,9 @@
+package com.dalongtech.testapplication.app;
+
+/**
+ * Author:xianglei
+ * Date: 2019-12-14 11:43
+ * Description:
+ */
+public class App {
+}
---
+++ dataanalysis/src/test/java/com/dalongtech/analysis/AnalysisAgentTest.java
@@ -0,0 +1,9 @@
+package com.dalongtech.analysis;
+
+/**
+ * Author:xianglei
+ * Date: 2019-12-14 11:14
+ * Description:
+ */
+public class AnalysisAgentTest {
+}
