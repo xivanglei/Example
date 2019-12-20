933550a6ec3c91e9e922bf6baf9c822073e4268f
xianglei <xianglei19881119@163.com>
2019-12-20 16:23:07 +0800

    增加测试页

---
+++ app/src/main/java/com/dalongtech/testapplication/activity/TestActivity.java
@@ -0,0 +1,19 @@
+package com.dalongtech.testapplication.activity;
+
+import android.os.Bundle;
+
+import com.dalongtech.testapplication.R;
+import com.dalongtech.testapplication.base.SimpleActivity;
+
+public class TestActivity extends SimpleActivity {
+
+    @Override
+    protected int getLayoutById() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void initViewAndData(Bundle savedInstanceState) {
+
+    }
+}
