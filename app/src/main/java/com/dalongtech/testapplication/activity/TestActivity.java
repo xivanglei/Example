003b1ec4452fd043e25f22a5a60ab7fc757d2958
xianglei <xianglei19881119@163.com>
2019-12-20 16:54:52 +0800

    小测试

--- app/src/main/java/com/dalongtech/testapplication/activity/TestActivity.java
+++ app/src/main/java/com/dalongtech/testapplication/activity/TestActivity.java
@@ -1,12 +1,18 @@
 package com.dalongtech.testapplication.activity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import com.dalongtech.testapplication.R;
 import com.dalongtech.testapplication.base.SimpleActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class TestActivity extends SimpleActivity {
 
+    @BindView(R.id.cb_test)
+    CheckBox cb_test;
     @Override
     protected int getLayoutById() {
         return R.layout.activity_test;
@@ -15,5 +21,10 @@ public class TestActivity extends SimpleActivity {
     @Override
     protected void initViewAndData(Bundle savedInstanceState) {
 
+    }
+
+    @OnClick(R.id.fl_test)
+    public void testCb() {
+        cb_test.setChecked(!cb_test.isChecked());
     }
 }
