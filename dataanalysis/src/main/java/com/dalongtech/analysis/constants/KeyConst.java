20ed17072d5f28cc5f004031b1c03a50c9a8938b
xianglei <xianglei19881119@163.com>
2019-12-30 14:37:23 +0800

    存储

---
+++ dataanalysis/src/main/java/com/dalongtech/analysis/constants/KeyConst.java
@@ -0,0 +1,9 @@
+package com.dalongtech.analysis.constants;
+
+/**
+ * Author:xianglei
+ * Date: 2019-12-30 14:34
+ * Description:
+ */
+public interface KeyConst {
+}
---
+++ dataanalysis/src/main/java/com/dalongtech/analysis/constants/TypeConst.java
@@ -0,0 +1,9 @@
+package com.dalongtech.analysis.constants;
+
+/**
+ * Author:xianglei
+ * Date: 2019-12-30 14:35
+ * Description:
+ */
+public interface TypeConst {
+}
